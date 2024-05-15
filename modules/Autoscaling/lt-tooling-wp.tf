e3771c95b3740fc3574599493ca42a9b827c9527
Quadri Bello <moyor_bello@yahoo.co.uk>
2024-05-15 04:39:03 +0100

    changeddd

--- modules/Autoscaling/lt-tooling-wp.tf
+++ modules/Autoscaling/lt-tooling-wp.tf
@@ -36,37 +36,37 @@ resource "aws_launch_template" "wordpress-launch-template" {
 
 
 # launch template for toooling
-resource "aws_launch_template" "tooling-launch-template" {
-  image_id               = var.ami-web
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = var.web-sg
+# resource "aws_launch_template" "tooling-launch-template" {
+#   image_id               = var.ami-web
+#   instance_type          = "t2.micro"
+#   vpc_security_group_ids = var.web-sg
 
-  iam_instance_profile {
-    name = var.instance_profile
-  }
+#   iam_instance_profile {
+#     name = var.instance_profile
+#   }
 
-  key_name = var.keypair
-
-
-  placement {
-    availability_zone = "random_shuffle.az_list.result"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tag_specifications {
-    resource_type = "instance"
+#   key_name = var.keypair
 
 
- tags = merge(
-    var.tags,
-    {
-      Name = "tooling-launch-template"
-    },
-  )
-  }
+#   placement {
+#     availability_zone = "random_shuffle.az_list.result"
+#   }
 
-  user_data = filebase64("${path.module}/tooling.sh")
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+
+#   tag_specifications {
+#     resource_type = "instance"
+
+
+#  tags = merge(
+#     var.tags,
+#     {
+#       Name = "tooling-launch-template"
+#     },
+#   )
+#   }
+
+#   user_data = filebase64("${path.module}/tooling.sh")
+# }
