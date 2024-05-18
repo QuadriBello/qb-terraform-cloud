558c3001c0e921e513ce7c0454875aa8dbc9470a
Quadri Bello <moyor_bello@yahoo.co.uk>
2024-05-18 20:22:48 +0100

    uncomment

--- modules/VPC/routes.tf
+++ modules/VPC/routes.tf
@@ -19,8 +19,6 @@ resource "aws_route" "private-rtb-route" {
 }
 
 # associate all private subnets to the private route table
-resource "aws_route_table_association" "private-subnets-assoc" {
-  count          = length(aws_subnet.private[*].id)
   subnet_id      = element(aws_subnet.private[*].id, count.index)
   route_table_id = aws_route_table.private-rtb.id
 }
