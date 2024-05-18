7574bc2c903f77155aaec8287dac1ac2b9fe9c1c
Quadri Bello <moyor_bello@yahoo.co.uk>
2024-05-18 04:29:26 +0100

    2nd attempt

--- main.tf
+++ main.tf
@@ -21,15 +21,15 @@
 #   }
 # }
 
-# resource "aws_dynamodb_table" "terraform_locks" {
-#   name         = "terraform-locks"
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "LockID"
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-# }
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
 
 
 # creating VPC
